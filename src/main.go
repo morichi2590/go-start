34c35ccf110fb6381d4544784e2de48f7e979468
morichi2590 <s.zero.s2590@gmail.com>
2022-04-04 23:16:11 +0900

    Update main.go

--- src/main.go
+++ src/main.go
@@ -31,6 +31,7 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request){
         Article{Title: "vue", Desc: "vue learning", Content: "i think simple"},
         Article{Title: "design", Desc: "design learn", Content: "i bought design book"},
         Article{Title: "enginner", Desc: "saw enginner friend", Content: "update skill"},
+        Article{Title: "kansai slary low", Desc: "very low", Content: "what tokyo very high?"},
     }
     fmt.Println("Endpoint Hit: returnAllArticles")
     json.NewEncoder(w).Encode(Articles)
